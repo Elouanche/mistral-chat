d4ba262066a7a5083c94d152e0ba37ecf460ef6a
Elouan CHE <elouan.chedalleux@ynov.com>
2025-04-15 14:12:57 +0200

    Add init project

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <header>
+        <h1>SALUT</h1>
+    </header>
+
+
+    <main>
+        <article>
+        <h2>OK</h2>
+        <p>bonjour</p>
+        </article>
+
+        <a href="/">OKOK</a>
+    </main>
+
+    <footer>
+    <p>Test</p>
+    </footer>
+</body>
+</html>
