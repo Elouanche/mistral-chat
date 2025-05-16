aa41686ddadb53db105eb42f887dc131dfb5e2da
JessyPiTech <j.piquerel.p@gmail.com>
2025-05-16 16:18:03 +0200

    updloadv2

--- DIR.php
+++ DIR.php
@@ -2,6 +2,7 @@
 define('BASE_PATH', __DIR__);
 define('PUBLIC_PATH', BASE_PATH . '/public/');
 define('SHARED_PATH', BASE_PATH . '/shared/');
+
 define('COMPONENT_PATH', BASE_PATH . '/front-end/Component/');
 define('STATIC_PATH', PUBLIC_PATH . 'static/'); // Note : Pas de `/` supplémentaire'COMPONENT_PATH', BASE_PATH . '/front-end/Component/');
 define('BASE_URL', (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]");
@@ -12,3 +13,5 @@ define('PRODUCT_IMAGES_URL', STATIC_URL . 'product_images/');
 define('CONFIG_PATH', BASE_PATH . '/config/');
 define('API_PATH', BASE_PATH . '/api/');
 define('SERVICES_PATH', BASE_PATH . '/services/');
+define('CRUD_PATH', BASE_PATH . '/CRUD/');
+define('SERVICE_CRUD_PATH', BASE_PATH . '/SERVICE_CRUD/');
